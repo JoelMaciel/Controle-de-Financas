5e621e8cc33908cdb1591431ebf44816709bef99
Joel Maciel <soldado25461@gmail.com>
2021-11-11 11:07:29 -0300

    carregando transacoes

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { GlobalStyle } from "./styles/global";
 import Modal from "react-modal";
 import { useState } from "react";
 import { NewTransactionModal } from "./components/NewTransactionModal";
-import { TransactionsContext } from "./TransactionsContext";
+import {  TransactionsProvider } from "./TransactionsContext";
 
 Modal.setAppElement("#root");
 
@@ -18,7 +18,7 @@ export function App() {
     setIsNewTransactionModal(false);
   }
   return (
-    <TransactionsContext.Provider value={[]}>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
       <NewTransactionModal
@@ -26,6 +26,6 @@ export function App() {
       onRequestClose={handleCloseNewTransactionModal }
       />
       <GlobalStyle />
-    </TransactionsContext.Provider>
+      </TransactionsProvider>
   );
 }
